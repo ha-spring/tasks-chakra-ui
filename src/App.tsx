bb372cb1909606cbeac3f74fe33b66c36e85c05a
ha-spring <spring4boot@gmail.com>
2023-07-17 15:24:54 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import RootLayout from "./layouts/RootLayout";
+import Dashboard from "./pages/Dashboard";
+import Create from "./pages/Create";
+import Profile from "./pages/Profile";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path="create" element={<Create />} />
+      <Route path="profile" element={<Profile />} />
+    </Route>,
+  ),
+);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import {
+  Flex,
+  Heading,
+  Spacer,
+  Box,
+  Text,
+  Button,
+  HStack,
+} from "@chakra-ui/react";
+
+export default function Navbar() {
+  return (
+    <Flex as="nav" p="10px" alignItems="center">
+      <Heading as="h1">Tasks</Heading>
+      <Spacer />
+
+      <HStack spacing="20px">
+        <Box bg="gray.200" p="10px">
+          M
+        </Box>
+        <Text>mario@gmail.com</Text>
+        <Button>Logout</Button>
+      </HStack>
+    </Flex>
+  );
+}
